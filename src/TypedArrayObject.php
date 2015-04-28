8e1eabd3a6424e6f3d61c36347205523a8245c92
Renaat De Muynck <renaat.demuynck@gmail.com>
2015-04-28 12:42:29 +0200

    Add type check when adding items to the array

--- src/TypedArrayObject.php
+++ src/TypedArrayObject.php
@@ -15,4 +15,40 @@ class TypedArrayObject extends \ArrayObject
     {
         $this->type = $type;
     }
+
+    private function isValid($value)
+    {
+        switch ($this->type) {
+            case 'bool':
+                return is_bool($value);
+            case 'int':
+                return is_int($value);
+            case 'float':
+                return is_float($value);
+            case 'string':
+                return is_string($value);
+            case 'array':
+                return is_array($value);
+            case 'resource':
+                return is_resource($value);
+            case 'callable':
+                return is_callable($value);
+            default:
+                return is_a($value, $this->type);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws \InvalidArgumentException If the given value was not of the specified type
+     */
+    public function offsetSet($offset, $value)
+    {
+        if ($this->isValid($value)) {
+            parent::offsetSet($offset, $value);
+        } else {
+            throw new \InvalidArgumentException('The given value was not of type ' . $this->type);
+        }
+    }
 }
